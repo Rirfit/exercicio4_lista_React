bbc5a973b00b6f99862c3ce146963cbc5a2e4c5b
Rirfit <lilrirfit@gmail.com>
2024-10-20 11:05:49 -0300

    FC

---
+++ exercicio4/src/components/Imagem.jsx
@@ -0,0 +1,7 @@
+function Imagem(){
+    return (
+        <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoP0cI8qLMBdtmv8BjNx1adssStpT25ptVbw&s" alt="PROA" />
+        </div>
+    )
+}export default Imagem
--- exercicio4/src/main.jsx
+++ exercicio4/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+import Imagem from './components/Imagem'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Imagem/>
   </StrictMode>,
 )
